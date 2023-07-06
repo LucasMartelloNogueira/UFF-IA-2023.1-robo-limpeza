c14a14dc314730ff886c0b1bc47c2583a24cfff6
Valesca Moura <valescamoura@id.uff.br>
2023-07-06 01:07:32 -0300

    Script para geração da base de fatos.

--- matriz.py
+++ matriz.py
@@ -1,4 +1,95 @@
-matriz = [
-    ["*", "*", "*", "O", "S"],
-    ["*", "*"]
+from typing import List, Tuple
+
+def is_there_any_obstacle(elem: str) -> bool:
+    if elem == 'O':
+        return True
+    return False
+
+def is_there_any_dirty(elem: str) -> bool:
+    if elem == 'S':
+        return True
+    return False
+
+def generate_edge_list(matrix: List[List[str]]) -> List[str]:
+    n = len(matrix)
+    m = len(matrix[0])
+    edges = []
+
+    def add_edge(cell_id, neighbor_id):
+        edges.append('aresta(1, {}, {}).' .format(cell_id, neighbor_id))
+
+    for i in range(n):
+        for j in range(m):
+            cell_id = i * m + j
+            cell = matrix[i][j]
+
+            # Vizinho acima
+            if i > 0:
+                neighbor_id = (i - 1) * m + j
+                add_edge(cell_id, neighbor_id)
+
+            # Vizinho abaixo
+            if i < n - 1:
+                neighbor_id = (i + 1) * m + j
+                add_edge(cell_id, neighbor_id)
+
+            # Vizinho à esquerda
+            if j > 0:
+                neighbor_id = i * m + (j - 1)
+                add_edge(cell_id, neighbor_id)
+
+            # Vizinho à direita
+            if j < m - 1:
+                neighbor_id = i * m + (j + 1)
+                add_edge(cell_id, neighbor_id)
+
+    return edges
+
+def generate_dirt_and_obstacles_list(matrix: List[List[str]]) -> Tuple[List[str], List[str]]:
+    n = len(matrix)
+    m = len(matrix[0])
+    dirt_list = []
+    obstacles_list = []
+
+    for i in range(n):
+        for j in range(m):
+            cell_id = i * m + j
+            cell = matrix[i][j]
+            if cell == 'O':
+                obstacles_list.append(f'obstaculo({cell_id}).')
+            elif cell == 'S':
+                dirt_list.append(f'sujeira({cell_id}).')
+
+    return dirt_list, obstacles_list
+
+def persist_facts(edges_list: List[str], obstacles_list: List[str], dirt_list: List[str], filename: str = 'tabuleiro.pl') -> None:
+    with open(filename, 'w') as file:
+        file.write('%% arestas\n')
+        for edge in edges_list:
+            file.write(edge + '\n')
+        file.write('\n')
+
+        file.write('%% obstaculos\n')
+        for obstacle in obstacles_list:
+            file.write(obstacle + '\n')
+        file.write('\n')
+
+        file.write('%% sujeiras\n')
+        for dirt in dirt_list:
+            file.write(dirt + '\n')
+
+    print('Base de fatos salva em: ', filename)
+
+
+matrix = [
+    ['*', '*', '*', 'O', 'S'],
+    ['*', '*', '*', '*', '*']
 ]
+
+edges_list = generate_edge_list(matrix)
+dirt_list, obstacles_list = generate_dirt_and_obstacles_list(matrix)
+persist_facts(edges_list, obstacles_list, dirt_list)
+
+# for edge in edge_list:
+#     print(edge)
+# print(dirt_list, obstacles_list)
---
+++ tabuleiro.pl
@@ -0,0 +1,33 @@
+%% arestas
+aresta(1, 0, 5).
+aresta(1, 0, 1).
+aresta(1, 1, 6).
+aresta(1, 1, 0).
+aresta(1, 1, 2).
+aresta(1, 2, 7).
+aresta(1, 2, 1).
+aresta(1, 2, 3).
+aresta(1, 3, 8).
+aresta(1, 3, 2).
+aresta(1, 3, 4).
+aresta(1, 4, 9).
+aresta(1, 4, 3).
+aresta(1, 5, 0).
+aresta(1, 5, 6).
+aresta(1, 6, 1).
+aresta(1, 6, 5).
+aresta(1, 6, 7).
+aresta(1, 7, 2).
+aresta(1, 7, 6).
+aresta(1, 7, 8).
+aresta(1, 8, 3).
+aresta(1, 8, 7).
+aresta(1, 8, 9).
+aresta(1, 9, 4).
+aresta(1, 9, 8).
+
+%% obstaculos
+obstaculo(3).
+
+%% sujeiras
+sujeira(4).
