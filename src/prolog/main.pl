fad3e12087b740505ef360ad41d24aa42f2f4aca
Lucas Martello Nogueira <lucas.martello0509@gmail.com>
2023-07-13 18:52:11 -0300

    add funcionalidade de pegar tempo de execução dos algoritmos de busca

--- src/prolog/main.pl
+++ src/prolog/main.pl
@@ -10,26 +10,49 @@
 
 :- consult('robo.pl').
 
-main(aEstrela, VerticeInicial, VerticeFinal):-
+/* main
+
+faz o setup e faz com que o robo percorra a sala, limpe as sujeiras e vá até o ponto final
+
++ <arg-1> : algoritmo de busca usado. Pode ser entre os seguintes (aEstrela, hillClimb, bestFirst e branchAndBound)
++ <arg-2> : vértice inicial do robô, ponto de partida para algortimo de busca
++ <arg-2> : vértice final, ponto de chegada para o robô
+- <arg-3> : tempo que a função demorou para rodar, em milisegundos (ms)
+
+*/
+
+main(aEstrela, VerticeInicial, VerticeFinal, Time):-
 	listaSujeirasOrdenadas(VerticeInicial, SujeirasOrdenadas),
 	resetaRobo(VerticeInicial),
+	statistics(walltime, [T0, _]),
 	roboLimpaSala(SujeirasOrdenadas, VerticeFinal, aEstrela),
-	removeSujeiras(SujeirasOrdenadas).
+	statistics(walltime, [T2, _]),
+	removeSujeiras(SujeirasOrdenadas),
+	Time is T2 - T0.
 
-main(hillClimb, VerticeInicial, VerticeFinal) :-
+main(hillClimb, VerticeInicial, VerticeFinal, Time):-
 	listaSujeirasOrdenadas(VerticeInicial, SujeirasOrdenadas),
 	resetaRobo(VerticeInicial),
+	statistics(walltime, [T0, _]),
 	roboLimpaSala(SujeirasOrdenadas, VerticeFinal, hillClimb),
-	removeSujeiras(SujeirasOrdenadas).
+	statistics(walltime, [T2, _]),
+	removeSujeiras(SujeirasOrdenadas),
+	Time is T2 - T0.
 
-main(bestFirst, VerticeInicial, VerticeFinal) :-
+main(bestFirst, VerticeInicial, VerticeFinal, Time):-
 	listaSujeirasOrdenadas(VerticeInicial, SujeirasOrdenadas),
 	resetaRobo(VerticeInicial),
+	statistics(walltime, [T0, _]),
 	roboLimpaSala(SujeirasOrdenadas, VerticeFinal, bestFirst),
-	removeSujeiras(SujeirasOrdenadas).
+	statistics(walltime, [T2, _]),
+	removeSujeiras(SujeirasOrdenadas),
+	Time is T2 - T0.
 
-main(branchAndBound, VerticeInicial, VerticeFinal) :-
+main(branchAndBound, VerticeInicial, VerticeFinal, Time):-
 	listaSujeirasOrdenadas(VerticeInicial, SujeirasOrdenadas),
 	resetaRobo(VerticeInicial),
+	statistics(walltime, [T0, _]),
 	roboLimpaSala(SujeirasOrdenadas, VerticeFinal, branchAndBound),
-	removeSujeiras(SujeirasOrdenadas).
+	statistics(walltime, [T2, _]),
+	removeSujeiras(SujeirasOrdenadas),
+	Time is T2 - T0.
